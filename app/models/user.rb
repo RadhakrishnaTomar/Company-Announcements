29125b288357bd2c0e60c2b74c3b797821a8810d
RadhakrishnaTomar <radhakrishnarajput94@gmail.com>
2024-08-10 23:27:59 +0530

    add the model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+    validates :full_name, :address, presence: true
+    validates :mobile_number, presence: true, numericality: { only_integer: true }, length: { is: 10 }
 end
